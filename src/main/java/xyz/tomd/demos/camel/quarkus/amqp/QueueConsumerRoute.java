ae2a41daea4e4b7a0b1ffc3a2676bd3d5e31bf7a
Tom Donohue <monodot@gmail.com>
2020-08-17 11:01:27 +0100

    Typo

--- src/main/java/xyz/tomd/demos/camel/quarkus/amqp/QueueConsumerRoute.java
+++ src/main/java/xyz/tomd/demos/camel/quarkus/amqp/QueueConsumerRoute.java
@@ -8,7 +8,7 @@ public class QueueConsumerRoute extends RouteBuilder {
     public void configure() throws Exception {
         // Camel JMS component detects the ConnectionFactory
         // which is created by the Qpid JMS Quarkus extension
-        from("jms:queue:ALEX.HONKING")
+        from("jms:queue:ALEX.BIRD")
                 .log("Honk honk! I just received a message: ${body}");
     }
 
